1db7f11aca899d56abbaec2d65c6cd18408e673a
Lauren tau <taulauren@gmail.com>
2025-10-26 14:40:32 +0200

    creating  components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+ function Header() {
+  return (
+    <div className='flex justify-around'>
+    <div> home</div>
+    <div>About</div>
+    <div>Project</div>
+    <div>Contact</div>
+    </div>
+  )
+}
+export default Header
